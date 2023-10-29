b8260a73141ed7319d4f5efcf2698ca4abe5464e
Marian Schubert <marian.schubert@gmail.com>
2023-10-29 21:29:39 +0100

    init

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.9.10"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("com.approvaltests:approvaltests:22.2.0")
+    testImplementation("net.jqwik:jqwik:1.8.1")
+}
+
+tasks {
+    test {
+        useJUnitPlatform {
+            includeEngines("jqwik", "junit-jupiter")
+        }
+
+        testLogging.exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        testLogging.showCauses = true
+    }
+}
